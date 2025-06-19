80b2b98f20b61283347f197e0dfb697cdde91d0d
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2025-06-19 17:40:26 +0300

    solution

--- prisma/migrations/20250617135342_add/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "user" ADD COLUMN     "reset_password_token" TEXT;
--- prisma/migrations/20250618135549_add/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "user" ADD COLUMN     "change_email_token" TEXT,
-ADD COLUMN     "new_email" TEXT;
